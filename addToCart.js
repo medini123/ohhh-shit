3d51546534eb8cf3c077d4273c6b74317adaea09
medini123 <115324443+medini123@users.noreply.github.com>
2023-04-29 10:09:28 +0200

    update

--- addToCart.js
+++ addToCart.js
@@ -4,18 +4,21 @@ const product = [
     image: "/images/surgam.jpg",
     title: "surgam 300mg",
     price: 10.5,
+    link: "/Produit3.html",
   },
   {
     id: 1,
     image: "/images/3COARTS180C2FRLR-2.jpg",
     title: "Co-Artesiane 60 ml",
     price: 17.5,
+    link: "/Produit4.html",
   },
   {
     id: 2,
     image: "/images/petit-chene-croissance-sirop-fl-125ml.png",
     title: "Petit Chêne Croissance Sirop ",
     price: 22.5,
+    link: "/Produit.html",
   },
   {
     id: 3,
@@ -23,18 +26,21 @@ const product = [
       "/images/doliprane-liquiz-douleurs-et-fievre-1000-mg-8-sachets-suspension-buvable.jpg",
     title: "DOLIPRANE 1000 mg ",
     price: 3.5,
+    link: "/Produit1.html",
   },
   {
     id: 4,
     image: "/images/3001108.jpg",
     title: "LYSOPAINE ambroxol",
     price: 8.5,
+    link: "/Produit2.html",
   },
   {
     id: 5,
     image: "/images/76117746a55ab6fabf2b528bf1869afdf9487f43.jpg",
     title: "Moderna COVID-19 Vaccine",
     price: 10.5,
+    link: "/Produit5.html",
   },
 ];
 const categories = [
@@ -44,26 +50,44 @@ const categories = [
     })
   ),
 ];
-function myFunction(product) {
-  if (product[0]) {
-    window.location.href = "/Produit3.html";
-  } else if (product[1]) {
-    window.location.href = "/Produit4.html";
-  } else if (product[2]) {
-    window.location.href = "/produit.html";
-  } else if (product[3]) {
-    window.location.href = "/produit1.html";
-  } else if (product[4]) {
-    window.location.href = "/produit2.html";
+
+// function myFunction() {
+//   if (product.link == "/Produit3.html") {
+//     window.location.href = "/Produit3.html";
+//   }
+//   // } else if (product == product[1]) {
+//   //   window.location.href = "/Produit4.html";
+//   // } else if (product == product[2]) {
+//   //   window.location.href = "/produit.html";
+//   // } else if (product == product[3]) {
+//   //   window.location.href = "/produit1.html";
+//   // } else if (product == product[4]) {
+//   //   window.location.href = "/produit2.html";
+//   // } else {
+//   //   window.location.href = "/produit5.html";
+//   // }
+// }
+function fonction() {
+  var name = prompt("Enter Your Name:");
+
+  if (name == null || name == "") {
+    prompt("You did not entert anything. Please enter your name again");
   } else {
-    window.location.href = "/produit5.html";
+    prompt("You enterted: " + name);
   }
+}
+function anounce() {
+  prompt("type yes ,if you want to quit this page !!");
+  alert(
+    "if you want to see details page press ok ",
+    (window.location.href = "/Produit3.html")
+  );
 }
 
 let i = 0;
 document.getElementById("root").innerHTML = categories
   .map((item) => {
-    var { image, title, price } = item;
+    var { image, title, price, link } = item;
     return (
       `
         <div class='img-box'>
@@ -71,10 +95,10 @@ document.getElementById("root").innerHTML = categories
 
         <p>${title}</p>
         <h2 class='aaaa'>${price}.00</h2> ` +
-      "<button class='butto' onclick='addtocart(" +
+      "<button class='butto' onclick='fonction() ; addtocart(" +
       i++ +
       ")'>Add to cart  </button>" +
-      "<button class='ffff' onClick='myFunction(id)'> Details</button>" +
+      "<button class='ffff' id='fff' onClick='anounce() ; myFunction()'' > Details</button>" +
       `
         </div>`
     );
@@ -121,3 +145,4 @@ function displaycart(a) {
       .join("");
   }
 }
+document.getElementById("fff").window.prompt;
